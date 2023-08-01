b3135888a3b7b09323a79553c07c7e3a84a378b5
Alisher Manatbek <alisher.manatbek2004@gmail.com>
2023-08-01 13:21:20 +0600

    Get Started

---
+++ MySkills/View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySkills
+//
+//  Created by Alisher Manatbek on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
